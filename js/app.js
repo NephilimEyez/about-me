bfc2122b7ba451d0eee5e0c40df37e6c02fbb878
NephilimEyez <34822464+NephilimEyez@users.noreply.github.com>
2023-05-03 11:57:11 -0400

    formatting in js notes needed a new line

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ const userNameInput = prompt('Welcome to the site! What\'s your name?').toLowerC
 // I found replace here: https://stackoverflow.com/questions/4993764/how-to-remove-numbers-from-a-string
 // I read more about it on w3schools here: https://www.w3schools.com/jsref/jsref_replace.asp
 // I played with it a bit and learned some of the basic functions, but the one I used I don't understand the command. I am still looking in to why it works.
-// After getting in to Mozillas description (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace), reading about RegExp and then searching the actual term, I now understand it, but I'm in awe of the person who came up with it!
+// After getting in to Mozillas description (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace), 
+// reading about RegExp and then searching the actual term, I now understand it, but I'm in awe of the person who came up with it!
 
 let userNameInProgress = userNameInput.replace(/\d+/g, '');
 
